766a8145191f2133440ca0086c42f972aafa1d0f
hasibur14 <hasib141415@gmail.com>
2024-05-20 00:50:15 +0600

    added the crat item in db using email

--- index.js
+++ index.js
@@ -32,18 +32,28 @@ async function run() {
         const cartCollection = client.db('bistroDB').collection('carts');
 
 
-
+        //MENU COLLECTIONS :
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray()
             res.send(result)
         });
 
 
+
+        //CARTS COLLECTIONS:
+
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email}
+            const result = await cartCollection.find(query).toArray()
+            res.send(result)
+        });
+
         app.post('/carts', async (req, res) => {
             const cartItem = req.body
             const result = await cartCollection.insertOne(cartItem)
             res.send(result)
-        })
+        });
 
 
         await client.db("admin").command({ ping: 1 });
