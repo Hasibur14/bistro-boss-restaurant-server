8467b94b3889009529ac22a120f71651f0df2fd0
hasibur14 <hasib141415@gmail.com>
2024-05-23 19:42:53 +0600

    added jwt

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-require('dotenv').config()
+const jwt = require('jsonwebtoken')
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -11,6 +12,7 @@ app.use(express.json())
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lfxjcnl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -32,7 +34,71 @@ async function run() {
         const reviewCollection = client.db('bistroDB').collection('reviews');
         const cartCollection = client.db('bistroDB').collection('carts');
 
-        //user related api
+
+        // jwt related api --generate jwt
+        app.post('/jwt', async (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '24h' })
+            res.send({ token })
+        });
+
+        //Middleware related api...
+        const verifyToken = (req, res, next) => {
+            console.log('inside verify token', req.headers.authorization)
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: ' unauthorized access' })
+                }
+                req.decoded = decoded
+                next()
+            })
+        };
+
+
+        //user verify admin after verify token
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.params.email
+            const query = { email: email }
+            const user = await userCollection.findOne(query)
+            const isAdmin = user?.role === 'admin';
+            if (!isAdmin) {
+                return res.status(403).send({ message: ' forbidden access' })
+            }
+            next()
+        };
+
+        /**
+         * ----------------------------------
+         *     USER RELATED API
+         * ----------------------------------
+         */
+
+        app.get('/users', verifyAdmin, verifyToken, async (req, res) => {
+            console.log(req.headers);
+            const result = await userCollection.find().toArray()
+            res.send(result)
+        });
+
+
+        app.get('/userss/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            const query = { email: email };
+            const user = await userCollection.findOne(query)
+            let admin = false;
+            if (user) {
+                admin = user?.role === 'admin'
+            }
+            res.send({ admin })
+        });
+
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             // insert email if user does not exits
@@ -46,7 +112,31 @@ async function run() {
 
         });
 
-        //MENU COLLECTIONS :
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query)
+            res.send(result)
+        });
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc)
+            res.send(result)
+        });
+
+
+        /**
+        * ----------------------------------
+        *          MENU COLLECTIONS
+        * ----------------------------------
+        */
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray()
             res.send(result)
@@ -58,7 +148,12 @@ async function run() {
             res.send(result);
         })
 
-        //CARTS COLLECTIONS:
+        /**
+        * ----------------------------------
+        *          CARTS COLLECTIONS:
+        * ----------------------------------
+        */
+
 
         app.get('/carts', async (req, res) => {
             const email = req.query.email;
